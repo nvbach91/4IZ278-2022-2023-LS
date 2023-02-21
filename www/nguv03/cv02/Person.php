537caa2d4209831ec4b0d7dc1ff77aac88ecc36a
Nguyen Viet Bach <nvbach91@outlook.com>
2023-02-21 17:59:32 +0100

    Update Person.php

--- www/nguv03/cv02/Person.php
+++ www/nguv03/cv02/Person.php
@@ -1,6 +1,49 @@
 <?php
 
+// PHP 8 constructor promotion
 class Person {
+    public function __construct(
+        private $avatar;
+        private $firstName;
+        private $lastName;
+        private $title;
+        private $company;
+        private $available;
+        private $phone;
+        private $email;
+        private $website;
+        private $street;
+        private $propNumber;
+        private $orientationNumber;
+        private $city;
+    ) {}
+
+    public function getAddress() {
+        return "$this->street $this->propNumber / $this->orientationNumber, $this->city";
+    }
+
+    public function getAvailability() {
+        return $this->available ? 'Not available for contracts' : 'Now available for contracts';
+    }
+}
+
+/*
+// PHP 7
+class Person {
+    private $avatar;
+    private $firstName;
+    private $lastName;
+    private $title;
+    private $company;
+    private $available;
+    private $phone;
+    private $email;
+    private $website;
+    private $street;
+    private $propNumber;
+    private $orientationNumber;
+    private $city;
+
     public function __construct($avatar, $firstName, $lastName, $title, $company, $available, $phone, $email, $website, $street, $propNumber, $orientationNumber, $city) {
         $this->avatar = $avatar;
         $this->firstName = $firstName;
@@ -24,5 +67,5 @@ class Person {
         return $this->available ? 'Not available for contracts' : 'Now available for contracts';
     }
 }
-
+*/
 ?>
