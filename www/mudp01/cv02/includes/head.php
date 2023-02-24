d3ab010c0f3d3f188df4fbc236860d6981a5b4e9
Nguyen Viet Bach <nvbach91@outlook.com>
2023-02-24 15:55:28 +0100

    Revert "mudp01 - cv02"

--- www/mudp01/cv02/includes/head.php
+++
@@ -1,8 +0,0 @@
-<head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Vizitky</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Labrada">
-<link rel="stylesheet" href="css/style.css">
-</head>
