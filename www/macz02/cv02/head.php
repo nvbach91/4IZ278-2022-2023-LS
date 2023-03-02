9d2eeddbb27bc7bca678d6bf191fef397551e01a
zmac11 <38797564+zmac11@users.noreply.github.com>
2023-03-02 22:54:24 +0100

    maz02 - cv02

---
+++ www/macz02/cv02/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible">
+        <meta name="viewport" content="width=device">
+        <title>Vizitky</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
