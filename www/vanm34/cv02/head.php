2e2cd65bb58b83691f0da826e2739f386721b208
Martin Váňa <115477491+Arkinsen@users.noreply.github.com>
2023-03-03 09:48:19 +0100

    vanm34-cv02
    odevzdávám cvičení 02

---
+++ www/vanm34/cv02/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <title>Business Card</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
