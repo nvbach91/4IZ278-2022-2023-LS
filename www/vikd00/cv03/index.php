414d3919020a3aabe3ac7a3b395104a9c65dd1a9
vikd00 <101962943+vikd00@users.noreply.github.com>
2023-03-07 10:57:30 +0100

    vikd00-cv03
    Vypracovane cv 03

---
+++ www/vikd00/cv03/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+
+<body>
+    <h1>PHP Form validation</h1>
+    <?php require './registration-form.php' ?>
+</body>
+
+</html>
