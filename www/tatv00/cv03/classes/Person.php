d1f4060dcb5cc2a94e731cd0c2ed3d60614cbd41
Vadym <85949628+utikpuhlik@users.noreply.github.com>
2023-03-08 18:55:11 +0100

    tatv00-cv03
    https://esotemp.vse.cz/~tatv00/cv03/index.php

---
+++ www/tatv00/cv03/classes/Person.php
@@ -0,0 +1,14 @@
+<?php
+Class person {
+    public function __construct(
+        public $name,
+        public $age
+
+    ){}
+}
+
+$person = [
+    'name' => 'Reece James',
+    'age' => 50
+];
+?>
