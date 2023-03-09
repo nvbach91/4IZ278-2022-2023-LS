d6c7f628f7ae3ddf246b02e86084f3a6208334f0
Dreameryyy <119299216+Dreameryyy@users.noreply.github.com>
2023-03-09 13:35:18 +0100

    jezf00-cv03
    Odevzdavam cviceni 03

---
+++ www/jezf00/cv03/includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form validation</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+
+    </header>
