518690acd7da10ea090a923f4c037eaddfc76a30
Petr Buk <91278935+PetrBuk@users.noreply.github.com>
2023-03-22 15:40:03 +0100

    bukp00-cv05
    Odevzdávám cv05

---
+++ www/bukp00/cv05/index.php
@@ -0,0 +1,9 @@
+<?php require './header.php'; ?>
+
+<main class='wrapper'>
+  <h1>Database Operations interface</h1>
+  <pre>
+  <?php require './database/database.php'; ?>
+  </pre>
+</main>
+<?php require './footer.php'; ?>
