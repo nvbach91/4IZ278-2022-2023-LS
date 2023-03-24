e4fd1c638da940f7046e8745d4d879c343e8ff33
Konstantin Pankratov <pankratovkm@gmail.com>
2023-03-24 09:11:49 +0100

    pank09-cv05

---
+++ www/pank09/cv05/classes/ProductsDB.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/AbstractClasses/AbstractDatabase.php';
+
+class ProductsDB extends Database
+{
+    public function create($args)
+    { 
+        echo 'Product ', $args['name'], ' $', $args['price'], ' was created', PHP_EOL; 
+    }
+    public function fetch()  { echo 'A product was fetched', PHP_EOL; }
+    public function save()   { echo 'A product was saved  ', PHP_EOL; }
+    public function delete() { echo 'A product was deleted', PHP_EOL; }
+}
