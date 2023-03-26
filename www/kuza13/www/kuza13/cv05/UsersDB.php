f1cfd0c56bcc718e62dab2ceb26623920f81cf1b
andreukuznec <92484311+andreukuznec@users.noreply.github.com>
2023-03-26 14:03:28 +0200

    kuza13-cv05
    https://esotemp.vse.cz/~kuza13/cv05/

---
+++ www/kuza13/www/kuza13/cv05/UsersDB.php
@@ -0,0 +1,10 @@
+<?php 
+require_once ('./Database.php');
+class usersDB extends Database {
+    public function create($args){
+        echo 'User ', $args['name'], ' age: ', $args['age'], ' was created', PHP_EOL; 
+    }
+    public function fetch()  { echo 'A user was fetched', PHP_EOL; }
+    public function save()   { echo 'A user was saved  ', PHP_EOL; }
+    public function delete() { echo 'A user was deleted', PHP_EOL; }
+}
