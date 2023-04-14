2b3a346b08c16bcfcf255dfbdb9451e13be0b9fe
birossamuel613 <126236324+birossamuel613@users.noreply.github.com>
2023-04-14 15:51:42 +0200

    aktualizacia cv06
    oprava cv06

--- www/birs03/cv06/config.php
+++ www/birs03/cv06/config.php
@@ -7,7 +7,7 @@
 
 const DB_HOST = 'localhost';
 const DB_DATABASE = 'birs03';
-const DB_USERNAME = 'birs03';
-const DB_PASSWORD = 'shooph7eegoh3aeXei';
+const DB_USERNAME = '';
+const DB_PASSWORD = '';
 
 ?>
