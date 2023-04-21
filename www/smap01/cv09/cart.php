36aa587b266822b0d55ea001f1ecda5451d6fa8a
smap01 <125578687+smap01@users.noreply.github.com>
2023-04-21 03:21:04 +0200

    cv09
    odevzdávám cv09
    url: https://esotemp.vse.cz/~smap01/cv09/

---
+++ www/smap01/cv09/cart.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . '/incl/header.php';
+require_once __DIR__ . '/database.php'; ?>
+<?php
+$database=new Database();
+$goods = isset($_SESSION['goods']) ? $_SESSION['goods'] : [];
+?>
+<main class="container" style="max-width: 60% !important;">
+    <h1 style="text-align:center;">Shopping cart</h1>
+    <table>
+        <tbody>
+            <tr>
+                <td width=200></td>
+                <td style="width:10%;margin-left:20px;">ID</td>
+                <td style="width:60%;margin-left:5%;">Name</td>
+                <td style="margin-left:5%;text-align:center;">Price</td>
+                <td style="width:10%;"></td>
+            </tr>
+    <?php
+    $item;
+    foreach ($goods as $good) {
+        $item=$database->getItem($good);
+        echo
+        '<tr style="border-color:gray!important;border-style:solid!important;border-radius:7px;border:1px;height:70px;margin-bottom:10px;" class="container-fluid">
+            <td width=200><img src="https://www.designingbuildings.co.uk/w/images/6/6f/Field-175959_640.jpg" style="max-width:100px; max-height:100%;" alt="item_picture"></td>
+            <td style="width:10%;margin-left:20px;">ID: '.$good.'</td>
+            <td style="width:60%;margin-left:5%;">'.$item['name'].'</td>
+            <td style="margin-left:5%;">'.$item['price'].'</td>
+            <td style="width:10%;"><a href="remove-item.php?good_id='.$good.'" style="width:100%;display:block;text-align:center;"><i class="btn btn-danger fa-solid fa-trash-can"></i></a></td>
+        </tr>';
+    }
+    ?>
+</main>
+<?php require_once __DIR__ . '/incl/footer.php'; ?>
---
+++ www/smap01/cv09/config.php
@@ -0,0 +1,8 @@
+<?php 
+
+const DB_HOST='localhost';
+const DB_DATABASE='cv09';
+const DB_USERNAME='root';
+const DB_PASSWORD='';
+
+?>
