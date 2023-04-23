40a64e9d49a3cd0eefaba6c9f95c6d6412ceed8b
root <root@production-ubuntu22.04>
2023-04-23 08:35:36 +0000

    fix css carousel for mobile

--- src/resources/views/checkout.blade.php
+++ src/resources/views/checkout.blade.php
@@ -67,42 +67,6 @@
       </symbol>
     </svg>
 
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-      <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
-              id="bd-theme"
-              type="button"
-              aria-expanded="false"
-              data-bs-toggle="dropdown"
-              aria-label="Toggle theme (auto)">
-        <svg class="bi my-1 theme-icon-active" width="1em" height="1em"><use href="#circle-half"></use></svg>
-        <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-      </button>
-      <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-            <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="#sun-fill"></use></svg>
-            Light
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
-            <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="#moon-stars-fill"></use></svg>
-            Dark
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
-            <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="#circle-half"></use></svg>
-            Auto
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-      </ul>
-    </div>
-
-
 <div class="container">
   <main>
     <div class="py-5 text-center">
@@ -113,25 +77,25 @@
 
     <div class="row g-5">
       <div class="col-md-5 col-lg-4 order-md-last">
-      <h4 class="d-flex justify-content-between align-items-center mb-3">
-        <span class="text-primary">Your cart</span>
-        <span class="badge bg-primary rounded-pill">{{ count($cart) }}</span>
-      </h4>
-      <ul class="list-group mb-3">
-          @foreach($cart as $item)
-          <li class="list-group-item d-flex justify-content-between lh-sm">
-              <div>
-                  <h6 class="my-0">{{ $item['name'] }}</h6>
-                  <small class="text-body-secondary">Brief description</small>
-              </div>
-              <span class="text-body-secondary">${{ $item['price'] }}</span>
-          </li>
-          @endforeach
-          <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>${{ number_format(array_sum(array_column($cart, 'price')), 2) }}</strong>
-          </li>
-      </ul>
+          <h4 class="d-flex justify-content-between align-items-center mb-3">
+              <span class="text-primary">Your cart</span>
+              <span class="badge bg-primary rounded-pill">{{ \App\Http\Controllers\CartController::cartItemCount() }}</span>
+          </h4>
+          <ul class="list-group mb-3">
+              @foreach($cart as $item)
+                  <li class="list-group-item d-flex justify-content-between lh-sm">
+                      <div>
+                          <h6 class="my-0">{{ isset($item['name']) ? $item['name'] : 'Unknown Product' }}</h6>
+                          <small class="text-body-secondary">Brief description</small>
+                      </div>
+                      <span class="text-body-secondary">${{ $item['price'] }}</span>
+                  </li>
+              @endforeach
+              <li class="list-group-item d-flex justify-content-between">
+                  <span>Total (USD)</span>
+                  <strong>${{ number_format(\Cookie::get('total', 0), 2) }}</strong>
+              </li>
+          </ul>
 
 
         <form class="card p-2">
