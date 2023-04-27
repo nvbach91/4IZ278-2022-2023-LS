cd3ad919ffcb37c78a9ebe79798d378cfb1c1759
root <root@production-ubuntu22.04>
2023-04-27 17:37:17 +0000

    update panel

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -16,6 +16,18 @@
                         {{ __('Your Orders') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('profile.edit')">
+                        {{ __('Profile') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('profile.edit')">
+                        {{ __('Admin Panel') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -37,7 +49,7 @@
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
-                        
+
                         <x-dropdown-link :href="route('orders')">
                             {{ __('Orders') }}
                         </x-dropdown-link>
