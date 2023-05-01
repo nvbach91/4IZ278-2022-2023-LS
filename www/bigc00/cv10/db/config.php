94b48704113f3a6a2b1cccfc91cc8d3a887637a5
bigc00 <126820314+bigc00@users.noreply.github.com>
2023-05-02 01:37:10 +0200

    bigc00-cv10
    https://esotemp.vse.cz/~bigc00/cv10/

---
+++ www/bigc00/cv10/db/config.php
@@ -0,0 +1,6 @@
+<?php
+    const DB_HOST = 'localhost';
+    const DB_DATABASE = 'bigc00';
+    const DB_USERNAME = '';
+    const DB_PASSWORD = '';
+?>
---
+++ www/bigc00/cv10/includes/main.php
@@ -0,0 +1,8 @@
+<body>
+    <form class>
+        <a href = 'registration.php'><input class='button' type='button' value='Registration'></a>
+        <br>
+        <a href = 'login.php'><input class='button' type='button' value='Login'></a>
+    </form>
+</body>
+</html>
