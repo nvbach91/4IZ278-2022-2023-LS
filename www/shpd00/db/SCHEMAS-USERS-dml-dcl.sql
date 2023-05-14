316c886be3e32d56f719e7f07e4e5d357f2c15ea
shpd00 <120758518+shpd00@users.noreply.github.com>
2023-05-14 23:57:29 +0200

    Delete SCHEMAS-USERS-dml-dcl.sql

--- www/shpd00/db/SCHEMAS-USERS-dml-dcl.sql
+++
@@ -1,7 +0,0 @@
-create schema DEVICE_OWNER;
-create schema USER_OWNER;
-create schema PHP_ETL_OWNER;
-
-create user php_user@localhost IDENTIFIED BY 'password';
-
-grant select on DEVICE_OWNER.* to php_user@localhost;
