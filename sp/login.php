aa7b1ebce3c4f893b063fa5ac25b75414732cc0d
Doky01 <84477745+Doky01@users.noreply.github.com>
2023-05-14 23:51:56 +0200

    dokt00-sp
    pilotni verze semestralni prace
    
    https://esotemp.vse.cz/~dokt00/sp/

---
+++ sp/login.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+
+// db
+$db_host = 'localhost';
+$db_name = 'tea_shop';
+$db_user = 'root';
+$db_password = '';
+
+// connection to database
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+// checking connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// user input from the form
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// user data from the database
+$sql = "SELECT user_id, password FROM Users WHERE username = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $hashed_password = $row['password'];
+
+    // verifying hashed password
+    if (password_verify($password, $hashed_password)) {
+        // password is correct
+        $_SESSION['user_id'] = $row['user_id'];
+        header("Location: logged_in.html");
+    } else {
+        // password incorrect
+        header("Location: index.html?error=incorrect_password");
+    }
+} else {
+    // username not found
+    header("Location: index.html?error=user_not_found");
+}
+
+$stmt->close();
+$conn->close();
+?>
