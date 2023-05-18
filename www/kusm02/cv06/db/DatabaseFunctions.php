34ceafe9f49f6fb09faa2bebb64557bcab425937
kusm02 <125608120+kusm02@users.noreply.github.com>
2023-05-18 18:18:27 +0200

    kusm02-sp
    Založení pull requestu pro semestrální práce

---
+++ www/kusm02/cv06/db/DatabaseFunctions.php
@@ -0,0 +1,11 @@
+<?php 
+
+interface DatabaseFunctions {
+    public function fetchAll();
+    public function fetchBy($field, $value);
+    public function create($args);
+    public function deleteBy($field, $value);
+    public function updateBy($conditions, $args);
+}
+
+?>
---
+++ www/kusm02/cv06/db/ProductDatabase.php
@@ -0,0 +1,20 @@
+<?php require_once __DIR__ . '/Database.php'; ?>
+<?php
+
+class ProductDatabase extends Database {
+    protected $tableName = 'products';
+    public function fetchByCategory($category_id) {
+        return $this->fetchBy('category_id', $category_id);
+    }
+    public function create($args) {
+        $sql = 'INSERT INTO ' . $this->tableName . '(name, price, img) VALUES (:name, :price, :img)';
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute([
+            'name' => $args['name'], 
+            'price' => $args['price'], 
+            'img' => $args['img'],
+        ]);
+    }
+}
+
+?>
