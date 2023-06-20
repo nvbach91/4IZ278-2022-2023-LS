7965f7e5dd4b1adb2d87067947162855da80cb69
Mykhailo Bubnov <applemix00@gmail.com>
2023-06-20 17:50:09 +0200

    Added cart, profile

---
+++ www/bubm02/sp/app/Http/Controllers/CartController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the profile for the given user.
+     */
+    public function add(Request $request)
+    {
+        $requestQuantity = $request->input('quantity');
+        if ($requestQuantity == null) {
+            $requestQuantity = 1;
+        }
+        $cart = $request->session()->get('cart');
+        if ($cart == null) {
+            $cart = [];
+        }
+        $itemId = $request->input('id');
+        if (isset($cart[$itemId])) {
+            $quantity = $cart[$itemId] + $requestQuantity;
+        } else {
+            $quantity = $requestQuantity;
+        }
+        $cart[$itemId] = $quantity;
+        $request->session()->put('cart', $cart);
+        return redirect()->route($request->input('redirect'));
+    }
+
+    public function subtract(Request $request)
+    {
+        $cart = $request->session()->get('cart');
+        if ($cart != null) {
+            $itemId = $request->input('id');
+            if (isset($cart[$itemId])) {
+                $quantity = $cart[$itemId] - 1;
+                if ($quantity <= 0) {
+                    unset($cart[$itemId]);
+                }
+                else {
+                    $cart[$itemId] = $quantity;
+                }
+            }
+            $request->session()->put('cart', $cart);
+        }
+        return redirect()->route($request->input('redirect'));
+    }
+
+    public function remove(Request $request)
+    {
+        $cart = $request->session()->get('cart');
+        if ($cart != null) {
+            $itemId = $request->input('id');
+            unset($cart[$itemId]);
+            $request->session()->put('cart', $cart);
+        }
+        return redirect()->route($request->input('redirect'));
+    }
+
+    /**
+     * Show the profile for the given user.
+     */
+    public function show(Request $request)
+    {
+        $cart = $request->session()->get('cart');
+        if (isset($cart))
+            $products = Item::all()->whereIn('id', array_keys($cart));
+        else
+            $products = [];
+        return view('cart', [
+            'cart' => $request->session()->get('cart'),
+            'items' => $products
+        ]);
+    }
+}
