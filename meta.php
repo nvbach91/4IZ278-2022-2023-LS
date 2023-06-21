53bc49d38979bc44960baf4440636d53c3b1c003
alex12202 <125646740+alex12202@users.noreply.github.com>
2023-06-21 09:39:14 +0200

    Delete meta.php

--- meta.php
+++
@@ -1,7 +0,0 @@
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="Vitajte v našom obchode KitchenStore ponúkame vám rôzne kuchynské potreby a spotrebiče.">
-      <meta name="keywords" content="eshop, kuchyňa, riad, spotrebiče">
-      <meta name="author" content="Alexander Adam">
-      <link rel="icon" type="image/png" href="pictures/icon.svg">
-      <link rel="stylesheet" type="text/css" href="styles/style.css">
