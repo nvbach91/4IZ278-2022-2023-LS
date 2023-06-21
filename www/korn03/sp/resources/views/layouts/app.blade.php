c9d1aed1d49034d7d5fc660934ab2630f060a23e
Nikita Korotov <korn03@vse.cz>
2023-06-21 20:04:14 +0200

    Avatar generation in profile via api

--- www/korn03/sp/resources/views/layouts/app.blade.php
+++ www/korn03/sp/resources/views/layouts/app.blade.php
@@ -77,12 +77,15 @@
                         @else
                             </li>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
+
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('profile') }}">
+                                        Profile </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
