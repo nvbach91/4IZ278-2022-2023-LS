df5e7c09fc42e42aaa075fe90722a8dba1c9ea10
MichK28 <114214178+MichK28@users.noreply.github.com>
2023-06-22 09:52:07 +0200

    Oprava 2

--- www/kulm11/sp/createOrder.php
+++ www/kulm11/sp/createOrder.php
@@ -19,26 +19,69 @@
         exit;
     }
 
+    if(!isset($_POST)){
+        header("Location: ./index.php");
+        exit;
+    }
     $user = $usersDB->getUserViaEmail($_COOKIE["username"]);
-    $itemsIDs = $_SESSION["cart"];
-    $totalprice = 0;
-    foreach($itemsIDs as $itemID){
-        $item = $itemsDB->fetch($itemID);
-        $totalprice = $totalprice + $item["price"];
+
+    if(array_key_exists('confirmButton', $_POST)){
+        $itemsIDs = $_SESSION["cart"];
+        $totalprice = 0;
+        foreach($itemsIDs as $itemID){
+            $item = $itemsDB->fetch($itemID);
+            $totalprice = $totalprice + $item["price"];
+        }
+
+        $ordersDB->createOrder(date("Y-m-d"), $totalprice, $user["userid"], htmlspecialchars($_POST["shipping"]), htmlspecialchars($_POST["payment"]));
+        $orderID = $ordersDB->getLastID();
+
+        $quantities = array_count_values($itemsIDs);
+        
+        foreach(array_unique($itemsIDs) as $temp){
+            $quantity=$quantities[$temp];
+            $itemsPrice=$quantity*$itemsDB->fetch($temp)["price"];
+            $item = $itemsDB->fetch($itemID);
+            $ordersDB->createOrderItem($quantity,$itemsPrice,$orderID,$temp,$item["name"]);
+        }
+
+        $_SESSION["cart"]=[];
+        header("Location: ./orderhistory.php");
+        exit;
     }
+?>
 
-    $ordersDB->createOrder(date("Y-m-d"), $totalprice, $user["userid"], htmlspecialchars($_POST["shipping"]), htmlspecialchars($_POST["payment"]));
-    $orderID = $ordersDB->getLastID();
+<!DOCTYPE html>
+<html lang="en">
 
-    $quantities = array_count_values($itemsIDs);
-    
-    foreach(array_unique($itemsIDs) as $temp){
-        $quantity=$quantities[$temp];
-        $itemsPrice=$quantity*$itemsDB->fetch($temp)["price"];
-        $item = $itemsDB->fetch($itemID);
-        $ordersDB->createOrderItem($quantity,$itemsPrice,$orderID,$temp,$item["name"]);
-    }
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Confirm order</title>
+</head>
 
-    $_SESSION["cart"]=[];
-    header("Location: ./orderhistory.php");
-    exit;
+<body>
+    <header>
+        <?php include "./includes/logo.php" ?>
+    </header>
+    <main id="confirmation">
+        <h2><strong>Is this your adress?</strong></h2>
+        <p><strong>First name</strong>: <?php echo $user["firstname"]; ?></p>
+        <p><strong>Last name</strong>: <?php echo $user["lastname"]; ?></p>
+        <p><strong>City</strong>: <?php echo $user["city"]; ?></p>
+        <p><strong>Street</strong>: <?php echo $user["street"]; ?></p>
+        <p><strong>Building number</strong>: <?php echo $user["buildingno"]; ?></p>
+        <p><strong>Zip code</strong>: <?php echo $user["zipcode"]; ?></p>
+        <form action="./createOrder.php" method="POST" id="checkoutForm">
+            <input type="text" name="shipping" value="<?php echo $_POST["shipping"];?>" hidden>
+            <input type="text" name="payment" value="<?php echo $_POST["payment"];?>" hidden>
+            <a href="./cart.php">Cancel</a>
+            <button type="submit" value="Confirm" name="confirmButton">Confirm</button>
+        </form>
+    </main>
+    <?php include "./includes/footer.php" ?>
+</body>
+
+</html>
